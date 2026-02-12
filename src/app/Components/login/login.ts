a0838ee5637c5b9bf1829872d998c03d1a632b18
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-02-12 10:23:56 +0530

    minor fixes

--- src/app/Components/login/login.ts
+++ src/app/Components/login/login.ts
@@ -65,7 +65,7 @@ ngOnDestroy() {
         this.successMessage = res.message;
 
         const role = res.data.role.toLowerCase();
-        this.cd.detectChanges();
+        
         localStorage.setItem('userName', res.data.name);
         localStorage.setItem('userRole', role);
 
@@ -95,9 +95,10 @@ ngOnDestroy() {
         } else {
           this.errorMessage = "Invalid credentials";
               }
-              this.cd.detectChanges();
+             
       }
     });
   }
 
 }
+
