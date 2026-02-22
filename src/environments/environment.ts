8dea47d89bd29f8ade5ad6d8368b4d8b35b3dbb3
Bg <bhavyagodhaniya2004@gmail.com>
2026-02-22 18:19:24 +0530

    url fixes , and UI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' //backend link
+  apiUrl: 'https://nonswimming-nonseriously-lester.ngrok-free.dev/api' //backend link
 };
