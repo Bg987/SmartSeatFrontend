bacee9b6c155f944c10452fb2941c9554ca965cd
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-05 19:11:57 +0530

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 let production = true;
 export const environment = {
-  apiUrl: production ?'https://smartseatbackend.onrender.com/api':"http://localhost:8080/api",
+  apiUrl: production ? 'https://smartseatbackend.onrender.com/api' : "http://localhost:8080/api",
+  
   apiUrl2 : production ? 'https://examportalsmartseatbackend.onrender.com/api': "http://localhost:8081/api",
 };
