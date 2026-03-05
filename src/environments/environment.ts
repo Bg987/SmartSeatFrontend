e02fa49c8adb526b8cfa895708b7f97efafb60dd
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-05 18:57:34 +0530

    url2 change for deploymemt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
-let production = true;
+let production = false;
 export const environment = {
   apiUrl: production ?'https://smartseatbackend.onrender.com/api':"http://localhost:8080/api",
-  apiUrl2 : "http://localhost:8081/api",
+  apiUrl2 : production ? 'https://examportalsmartseatbackend.onrender.com': "http://localhost:8081/api",
 };
