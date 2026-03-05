f66cae820bcdd2a7adeff1f199ad50fedd9d4b42
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-05 18:59:20 +0530

    depl. fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
-let production = false;
+let production = true;
 export const environment = {
   apiUrl: production ?'https://smartseatbackend.onrender.com/api':"http://localhost:8080/api",
   apiUrl2 : production ? 'https://examportalsmartseatbackend.onrender.com': "http://localhost:8081/api",
