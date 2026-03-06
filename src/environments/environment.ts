ff8a3768534ee356466bf6e93acc011eebe46c90
Bg <bhavyagodhaniya2004@gmail.com>
2026-03-06 13:54:59 +0530

    add proxy server url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 let production = true;
 export const environment = {
-  
-  apiUrl: production ? 'https://smartseatbackend.onrender.com/api' : "http://localhost:8080/api",
-  apiUrl2 : 'https://examportalsmartseatbackend.onrender.com/api',
+
+  apiUrl: production ? 'https://proxy-0xaq.onrender.com/api1' : "http://localhost:8080/api",
+  apiUrl2 :  production ?'https://proxy-0xaq.onrender.com/api2':"http://localhost:8081/api",
 };
